e2ad376acd8b1205c9669b4856e06f1b0cf90ac5
minjee <minjeeson.dev@gmail.com>
2024-02-27 18:22:24 +0000

    Exercise 2 : Compound Component

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,37 +4,46 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
   // 📜 https://react.dev/reference/react/Children
   // 📜 https://react.dev/reference/react/cloneElement
-  return <Switch on={on} onClick={toggle} />
+  return React.Children.map(children, child => {
+    if (allowedTypes.includes(child.type)) {
+      const newChild = React.cloneElement(child, {on, toggle})
+      return newChild
+    }
+    return child
+  })
 }
 
-// 🐨 Flesh out each of these components
+const ToggleOn = ({on, children}) => (on ? children : null)
+const ToggleOff = ({on, children}) => (!on ? children : null)
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
+const MyToggleButton = ({on}) => (on ? 'On' : 'Off')
 
-// Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
-
-// Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+const allowedTypes = [ToggleOn, ToggleOff, ToggleButton]
 
 function App() {
   return (
+    // <div>
+    //   <Toggle>
+    //     <ToggleOn>The button is on</ToggleOn>
+    //     <ToggleOff>The button is off</ToggleOff>
+    //     <ToggleButton />
+    //   </Toggle>
+    // </div>
+
+    // 1. 💯 Support DOM component children
     <div>
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <div>Hello</div>
         <ToggleButton />
+        <MyToggleButton />
       </Toggle>
     </div>
   )
