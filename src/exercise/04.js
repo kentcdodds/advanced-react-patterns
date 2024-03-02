124706b6e30b773d7dd93de9a06909932f86d495
minjee <minjeeson.dev@gmail.com>
2024-03-02 17:13:02 +0000

    Exercise 4 : Props Collections & Getters

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -8,20 +8,43 @@ function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  // Props Collections
+  const togglerProps = {
+    onClick: toggle,
+    on: on,
+    'aria-pressed': on,
+  }
+
+  // Props Getters
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: () => {
+        onClick && onClick()
+        toggle()
+      },
+      ...props,
+    }
+  }
+
+  return {on, toggle, togglerProps, getTogglerProps}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
+
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
-        {on ? 'on' : 'off'}
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
+        {!on ? 'on' : 'off'}
       </button>
     </div>
   )
